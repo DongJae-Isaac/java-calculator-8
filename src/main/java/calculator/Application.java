f4abdb1893876e0af3dc25db47c37250f8e65af0
DongJae Kim <ehdwo1227@naver.com>
2025-10-14 23:03:50 +0900

    refactor: 사용자로부터 문자열 입력받기(Console.readLine()) 클래스 구분

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,13 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // 사용자로부터 입력값 받기
-        String input = Console.readLine();
+        InputHandler inputHandler = new InputHandler();
+        String input = inputHandler.getInput();
+
+
 
 
     }
