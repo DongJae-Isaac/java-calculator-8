8457d713769850ea9ae593b64c408ba0c2ce257f
DongJae Kim <ehdwo1227@naver.com>
2025-10-14 22:40:02 +0900

    feat: 사용자로부터 문자열 입력받기(Console.readLine())

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -26,6 +26,8 @@ class ApplicationTest extends NsTest {
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        Application.main(new String[]{
+
+        });
     }
 }
