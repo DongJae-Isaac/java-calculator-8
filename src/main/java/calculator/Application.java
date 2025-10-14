69a7242e5a437fca422b30cf3024fc0e9f6b4729
DongJae Kim <ehdwo1227@naver.com>
2025-10-14 22:50:44 +0900

    feat: 사용자로부터 문자열 입력받기(Console.readLine())

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        // 사용자로부터 입력값 받기
         String input = Console.readLine();
 
 
