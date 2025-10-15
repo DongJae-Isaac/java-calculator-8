4befa456ac71cdd8e4ba2c9c132f0270e582cafa
DongJae Kim <ehdwo1227@naver.com>
2025-10-15 14:17:00 +0900

    test(SeparatorHandler): splitInputValue 테스트 코드 작성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,8 +7,26 @@ public class Application {
         InputHandler inputHandler = new InputHandler();
         String input = inputHandler.getInput();
 
+        // 빈 문자열 반환 시 0 출력
+        if (input.isEmpty()) {
+            System.out.println("0");
+        } else {
+            // input 받아서 구분자 처리
+            SeparatorHandler separatorHandler = new SeparatorHandler();
+            String[] splitString = separatorHandler.splitInputValue(input);
+
+            //출력 확인 테스트
+        for (String splitStr : splitString) {
+            System.out.println(splitStr);
+        }
+//            CalculateHandler calculateHandler = new CalculateHandler();
+            // 구분 된 문자열 숫자로 변환
 
 
+//            ResultPrinter resultPrinter = new ResultPrinter();
+            // 최종 결과 출력 위한 숫자 전달
 
+
+        }
     }
 }
