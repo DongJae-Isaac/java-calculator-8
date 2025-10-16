4039e18b75dc7489121c7eb562ae9545dcb48ae7
DongJae Kim <ehdwo1227@naver.com>
2025-10-17 02:22:34 +0900

    refactor(ResultPrinter): printer 메소드 출력문수정

--- src/main/java/calculator/ResultPrinter.java
+++ src/main/java/calculator/ResultPrinter.java
@@ -1,7 +1,7 @@
 package calculator;
 
 public class ResultPrinter {
-    public void printer(int result){
-        System.out.println(result);
+    public void printer(int result) {
+        System.out.println("결과 : " + result);
     }
 }
